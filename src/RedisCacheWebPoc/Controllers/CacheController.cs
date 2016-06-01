7a43aefdb110e477e3a8a339501d9a041b0a4e32
pfernandes <pfernandes@braspag.com.br>
2016-06-01 18:05:20 -0300

    Site.

---
+++ src/RedisCacheWebPoc/Controllers/CacheController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RedisCacheWebPoc.Model;
+using StackExchange.Redis;
+using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace RedisCacheWebPoc.Controllers
+{
+    public class CacheController : Controller
+    {
+        public IActionResult Add(int count)
+        {
+            var response = new StringBuilder();
+
+            for (int i = 0; i < count; i++)
+            {
+                var transaction = new TransactionModel
+                {
+                    TransactionId = Guid.NewGuid(),
+                    AcquirerTransactionId = i.ToString(),
+                    Amount = new Random(i).Next()
+                };
+
+                try
+                {
+                    var timer = new Stopwatch();
+                    timer.Start();
+                    Cache.StringSet(i.ToString(), JsonConvert.SerializeObject(transaction), expiry: new TimeSpan(0, 1, 0));
+                    timer.Stop();
+
+                    response.AppendFormat("#{0} added in {1}ms\r\n", i, timer.ElapsedMilliseconds);
+                }
+                catch (Exception)
+                {
+                    response.AppendFormat("#{0} error", i);
+                }
+            }
+
+            return Content(response.ToString());
+        }
+
+        private readonly Lazy<ConnectionMultiplexer> _lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+            ConnectionMultiplexer.Connect("transaction.redis.cache.windows.net:6380,password=AoAo9CkdCyUXn8fMYwX+6xbZvn3Mez98uEdy7aNZZJQ=,ssl=True,abortConnect=False"));
+
+        private IDatabase Cache => _lazyConnection.Value.GetDatabase();
+    }
+}
---
+++ src/RedisCacheWebPoc/Model/TransactionModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RedisCacheWebPoc.Model
+{
+    public class TransactionModel
+    {
+        public string AcquirerTransactionId { get; set; }
+        public long Amount { get; set; }
+        public string AuthorizationCode { get; set; }
+        public Guid? BraspagOrderId { get; set; }
+        public long? CapturedAmount { get; set; }
+        public DateTime ConfirmationPostNextTryDate { get; set; }
+        public short ConfirmationPostRemainingTries { get; set; }
+        public string Country { get; set; }
+        public string Currency { get; set; }
+        public Guid? CustomerId { get; set; }
+        public Guid? DeliveryAddressId { get; set; }
+        public string Eci { get; set; }
+        public string ErrorCode { get; set; }
+        public string ErrorMessage { get; set; }
+        public Dictionary<string, string> ExtraData { get; set; }
+        public DateTime? FinalizedDate { get; set; }
+        public int Installments { get; set; }
+        public bool IsSplitted { get; set; }
+        public Guid MerchantId { get; set; }
+        public string MerchantOrderId { get; set; }
+        public bool MustBeProbed { get; set; }
+        public DateTime? PaidDate { get; set; }
+        public Guid? ParentTransactionId { get; set; }
+        public short PaymentMethodCode { get; set; }
+        public Guid PaymentMethodId { get; set; }
+        public string PlanCode { get; set; }
+        public string ProofOfSale { get; set; }
+        public DateTime ReceivedDate { get; set; }
+        public string RequestIp { get; set; }
+        public string SentOrderId { get; set; }
+        public long ServiceTaxAmount { get; set; }
+        public DateTime? StartedDate { get; set; }
+        public Guid TransactionId { get; set; }
+        public long? VoidedAmount { get; set; }
+        public DateTime? VoidedDate { get; set; }
+    }
+}
